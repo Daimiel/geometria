fbdaa9f40225cc028687d45b19c4cd5d67e98739
Daniel Martinez <damavi84@gmail.com>
2020-02-01 17:08:35 +0100

    Version refactorizada

--- geometria/src/Rectangulo_DMV.java
+++ geometria/src/Rectangulo_DMV.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_DMV extends FiguraGeometrica_DMV {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_DMV(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
